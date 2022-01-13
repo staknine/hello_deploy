008d8db28ff4cdd8edd02f648d87e3aa281497ee
StakNine <staknine@protonmail.com>
2022-01-12 21:55:38 -0800

    Fix failing PageControllerTest

--- test/hello_deploy_web/controllers/page_controller_test.exs
+++ test/hello_deploy_web/controllers/page_controller_test.exs
@@ -1,6 +1,9 @@
 defmodule HelloDeployWeb.PageControllerTest do
   use HelloDeployWeb.ConnCase
 
+  import HelloDeploy.AccountsFixtures
+  setup :register_and_log_in_user
+
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
     assert html_response(conn, 200) =~ "Welcome to Phoenix!"
